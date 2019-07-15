f15269a388535a3c88c431a8044881cefacc9dac
Steven Villegas <sagrath23@gmail.com>
2019-07-15 00:42:04 -0500

    fetching data from service

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
 const PostLink = props => (
   <li>
@@ -9,8 +10,18 @@ const PostLink = props => (
   </li>
 );
 
-const Index = () => (
+const Index = (props) => (
   <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
     <p>Hello Next.js</p>
     <ul>
       <PostLink id="Hello Next.js" />
@@ -19,5 +30,16 @@ const Index = () => (
     </ul>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
 
 export default Index;
