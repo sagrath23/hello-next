e96fe3a687faeba584a5553643696b878be9807d
Steven Villegas <sagrath23@gmail.com>
2019-07-14 22:17:18 -0500

    add dynamic routing & remove compiled files

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);
 
 const Index = () => (
   <Layout>
     <p>Hello Next.js</p>
+    <ul>
+      <PostLink id="Hello Next.js" />
+      <PostLink id="Learn Next.js is awesome" />
+      <PostLink id="Deploy apps with Zeit" />
+    </ul>
   </Layout>
 );
 
